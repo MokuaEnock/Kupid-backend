346cdec18a66795025011227dcbf402eb6537225
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-05 14:55:07 +0300

    seed the users table with dummy data

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 # Kupid-backend
-
-
-
-   
-
-create_table :messages do |t|
-t.integer :user1_id
-t.integer :user2_id
-t.string :message
-t.timestamps
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
+20.times do
+  email = Faker::Internet.email
+  password = Faker::Lorem.word
+  User.create(email: email, password: password)
+end
 
 puts "✅ Done seeding!"
