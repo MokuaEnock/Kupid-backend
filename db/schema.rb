f4da3fbf082ef8e93b0179268e6dc1f8fc72e460
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-05 05:45:14 +0300

    create user bio migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_04_212425) do
+ActiveRecord::Schema.define(version: 2022_11_05_024343) do
+
+  create_table "bios", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date_of_birth"
+    t.string "gender"
+    t.string "country"
+    t.string "race"
+    t.integer "height"
+    t.string "skin_color"
+    t.string "pets"
+    t.string "sexual_orientation"
+    t.string "smoker"
+    t.string "drinker"
+    t.string "religion"
+    t.string "body_type"
+    t.string "wants_kids"
+    t.string "has_kids"
+    t.integer "number_of_kids"
+    t.string "divorced"
+    t.string "in_a_relationship"
+    t.string "occupation"
+    t.string "previous_relationship"
+  end
 
 end
