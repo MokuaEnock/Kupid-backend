cbc43a760795e45b5dc27bb1511c61c49d7879f7
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-11-05 05:46:48 +0300

    create user migration

--- README.md
+++ README.md
@@ -1,27 +1,6 @@
 # Kupid-backend
 
-create_table :bio do |t|
-t.string :name
-t.datetime :date_of_birth
-t.string :gender
-t.string :country
-t.string :race
-t.integer :height
-t.string :skin_color
-t.string :pets
-t.string :sexual_orientation
-t.string :smoker
-t.string :drinker
-t.string :religion
-t.string :body_type
-t.string :wants_kids
-t.string :has_kids
-t.integer :number_of_kids
-t.string :divorced
-t.string :in_a_relationship
-t.string :occupation
-t.string :previous_relationship
-end
+
 
        create_table :users do |t|
       t.string :email
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_05_024343) do
+ActiveRecord::Schema.define(version: 2022_11_05_024538) do
 
   create_table "bios", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 2022_11_05_024343) do
     t.string "in_a_relationship"
     t.string "occupation"
     t.string "previous_relationship"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
